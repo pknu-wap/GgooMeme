9f114a9eb4d69c016e1ce898b725ca588b55a8af
FhRh <yiyop@naver.com>
2024-03-27 00:55:35 +0900

    UserService

--- Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/controller/UserController.java
+++ Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/controller/UserController.java
@@ -6,6 +6,7 @@ import com.wap.GgoememeBackend.payload.UserDto;
 import com.wap.GgoememeBackend.repository.UserRepository;
 import com.wap.GgoememeBackend.security.CurrentUser;
 import com.wap.GgoememeBackend.security.UserPrincipal;
+import com.wap.GgoememeBackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,9 +31,10 @@ public class UserController {
 
     //GET /user/{userId}
     @GetMapping("/user/{userId}")
-    public ResponseEntity<?> getUserInfo(@CurrentUser UserPrincipal userPrincipal, @PathVariable String userId){
+    public ResponseEntity<?> getUserInfo(@CurrentUser UserPrincipal userPrincipal, @PathVariable Long userId){
+        UserDto userDto;
         try{
-            UserDto userDto = userService.findUser(userId);
+            userDto = userService.findUser(userId);
         }catch(Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
         }
