0f9fc7542f3068e55e8fe857da3e317137814a30
FhRh <yiyop@naver.com>
2024-03-30 00:08:48 +0900

    #7-PUT /post/bookmarks

--- Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/controller/PostController.java
+++ Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.NoSuchElementException;
@@ -27,5 +28,16 @@ public class PostController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
         }
         return new ResponseEntity<>(postDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/post/bookmark/{id}")
+    public ResponseEntity<String> clickBookmark(@CurrentUser UserPrincipal userPrincipal, @PathVariable("id") Long id){
+        String result;
+        try {
+            result= postService.clickBookmark(userPrincipal, id);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/service/PostService.java
+++ Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/service/PostService.java
@@ -4,6 +4,7 @@ import com.wap.GgoememeBackend.domain.Post;
 import com.wap.GgoememeBackend.domain.User;
 import com.wap.GgoememeBackend.payload.PostDto;
 import com.wap.GgoememeBackend.repository.PostRepository;
+import com.wap.GgoememeBackend.repository.UserRepository;
 import com.wap.GgoememeBackend.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,9 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
 
     public PostDto findById(UserPrincipal userPrincipal, Long id) throws NoSuchElementException{
         Post post = postRepository.findById(id)
@@ -25,11 +29,31 @@ public class PostService {
 
         //post의 유저중에 user의 id와 일치하는 user가 있는지 확인
         Set<User> users = post.getBookmarkedUsers();
-        boolean isBookMared = users.stream().anyMatch(user -> user.getId().equals(userPrincipal.getId()));
-        if(isBookMared){
+        boolean isBookMarked = users.stream().anyMatch(user -> user.getId().equals(userPrincipal.getId()));
+        if(isBookMarked){
             postDto.setBookmarked(true);
         }
 
         return postDto;
+    }
+
+    //TODO 쿼리 개선 필요
+    public String clickBookmark(UserPrincipal userPrincipal, Long id) throws RuntimeException{
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("no post"));
+
+        Set<User> users = post.getBookmarkedUsers();
+        User user = userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new RuntimeException("no user"));
+        boolean isBookMarked = users.stream().anyMatch(u -> u.equals(user));
+        if(isBookMarked){
+            post.getBookmarkedUsers().remove(user);
+            postRepository.save(post);
+            return "remove bookmark";
+        }else{
+            post.getBookmarkedUsers().add(user);
+            postRepository.save(post);
+            return "add bookmark";
+        }
     }
 }
