168ac18ce9fac5c2664b180b195c6762f4d08854
FhRh <yiyop@naver.com>
2024-03-29 17:59:32 +0900

    #7-더미데이터 추가

---
+++ Ggoememe-Backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO posts(id, image, likes)
+VALUES(1, "url", 1),
+      (2, "url", 2),
+      (3, "url", 3),
+      (4, "url", 4),
+      (5, "url", 5),
+      (6, "url", 6),
+      (7, "url", 7),
+      (8, "url", 8),
+      (9, "url", 9),
+      (10, "url",10);
