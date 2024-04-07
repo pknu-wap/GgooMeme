f51914a8f08969524845dace9b0d45ea1ff59390
Due_it <48638700+FhRh@users.noreply.github.com>
2024-04-08 00:00:49 +0900

    Revert "Feature/#15 reply"

--- Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/controller/PostController.java
+++ Ggoememe-Backend/src/main/java/com/wap/GgoememeBackend/controller/PostController.java
@@ -33,7 +33,7 @@ public class PostController {
         return new ResponseEntity<>(postDto, HttpStatus.OK);
     }
 
-    @PutMapping("/post/bookmark/{id}/{page}")
+    @PutMapping("/post/bookmark/{id}")
     public ResponseEntity<String> clickBookmark(@CurrentUser UserPrincipal userPrincipal, @PathVariable("id") Long id){
         String result;
         try {
